dd2abed7f7d439946327b38ae8c3dc114f136efb
Felippe Maurício <felippe.vieira@zoom.com.br>
2019-02-07 13:20:32 -0200

    Add storybook background

--- packages/components/.storybook/config.js
+++ packages/components/.storybook/config.js
@@ -1,11 +1,15 @@
 import { addDecorator, configure } from '@storybook/react';
+import { withBackgrounds } from '@storybook/addon-backgrounds';
 import { withThemesProvider } from 'storybook-addon-styled-component-theme';
 import { ThemeProvider } from 'styled-components';
 import Themes from '@react-components-lib-boilerplate/themes';
 
 
 addDecorator(withThemesProvider(Object.values(Themes)));
-
+addDecorator(withBackgrounds([
+  { name: 'Light', value: '#fff', default: true },
+  { name: 'Dark', value: '#000' },
+]));
 
 const req = require.context('../src', true, /.story.js$/);
 function loadStories() {
